96a512951d7c993e4174e98703d4922fc08a41c7
kotobukid <kotobuki@daqua.jp>
2024-07-05 16:25:50 +0900

    原始的に点を置いて線を描く
    ペイントツールという都合上、FillとStrokeがSVGで慣れた感じとは異なる点に注意

--- src/components/CanvasBackground.vue
+++ src/components/CanvasBackground.vue
@@ -3,23 +3,54 @@ import {onMounted, ref, inject, type Ref} from "vue";
 import type {BrushColor} from "../types.ts";
 
 const canvas = ref(null);
+let ctx: CanvasRenderingContext2D = null;
+
 const brushColor = inject('brush-color') as Ref<BrushColor>;
 
-onMounted(() => {
-  const ctx: CanvasRenderingContext2D = canvas.value.getContext("2d");
+let painting = false;
 
-  ctx.fillStyle = brushColor.value.fill;
-  ctx.strokeStyle = brushColor.value.stroke;
+onMounted(() => {
+  ctx = canvas.value.getContext("2d");
+
+  ctx.fillStyle = brushColor.value.stroke;
+  ctx.strokeStyle = 'transparent';
+  // ctx.strokeStyle = brushColor.value.fill;
   ctx.lineWidth = brushColor.value.strokeWidth;
 
   ctx.fillRect(25, 25, 100, 100);
   ctx.clearRect(45, 45, 60, 60);
   ctx.strokeRect(50, 50, 50, 50);
 });
+
+const start_painting = (e: PointerEvent) => {
+  painting = true;
+
+  ctx.beginPath();
+  ctx.arc(e.offsetX, e.offsetY, brushColor.value.strokeWidth / 2, 0, 2 * Math.PI);
+  ctx.fill();
+};
+
+const stop_painting = (e: PointerEvent) => {
+  painting = false;
+};
+
+const paint_move = (e: PointerEvent) => {
+  if (painting) {
+    ctx.beginPath();
+    ctx.arc(e.offsetX, e.offsetY, brushColor.value.strokeWidth / 2, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+};
 </script>
 
 <template lang="pug">
-  canvas.background(ref="canvas" width="1000" height="600")
+  canvas.background(
+    ref="canvas" width="1000" height="600"
+    @pointerdown="start_painting"
+    @pointerleave="stop_painting"
+    @pointerup="stop_painting"
+    @pointermove="paint_move"
+  )
 </template>
 
 <style scoped>
