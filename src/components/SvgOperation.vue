e331f19a1b739dfd3ff59b84c8dd744dc32063e3
kotobukid <kotobuki@daqua.jp>
2024-07-05 18:21:39 +0900

    SVG上に曲線を描く

--- src/components/SvgOperation.vue
+++ src/components/SvgOperation.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-
-import {computed, inject} from "vue";
+import {computed, inject, ref, type Ref} from "vue";
 import type {Point2D} from "../types.ts";
 
 const canvasSize = inject('canvas-size') as Point2D;
@@ -8,11 +7,56 @@ const canvasSize = inject('canvas-size') as Point2D;
 const viewBox = computed(() => {
   return `0 0 ${canvasSize.x} ${canvasSize.y}`;
 });
+
+let drawing = false;
+let points: Point2D[] = [];
+const points_to_draw: Ref<Point2D[]> = ref([]);
+
+const start_drawing = (e: PointerEvent) => {
+  drawing = true;
+  points = [{x: e.offsetX, y: e.offsetY}];
+};
+
+const stop_drawing = (e: PointerEvent) => {
+  if (drawing) {
+    drawing = false;
+    points.push({
+      x: e.offsetX,
+      y: e.offsetY
+    });
+    points_to_draw.value = [...points];
+  }
+};
+
+const move_draw = (e: PointerEvent) => {
+  if (drawing) {
+    points.push({
+      x: e.offsetX,
+      y: e.offsetY
+    });
+  }
+};
+
+const d = computed(() => {
+  return points_to_draw.value.map((point, index) => {
+    const prefix = index === 0 ? 'M' : 'L';
+    return `${prefix}${point.x},${point.y}`;
+  }).join(' ');
+});
 </script>
 
 <template lang="pug">
-  svg.operation(:width="canvasSize.x" :height="canvasSize.y" :viewBox="viewBox")
-    rect(x="50" y="50" width="50" height="50" fill="pink" stroke="black" stroke-width="2")
+  svg.operation(
+    :width="canvasSize.x" :height="canvasSize.y" :viewBox="viewBox"
+    @pointerdown="start_drawing"
+    @pointerup="stop_drawing"
+    @pointerleave="stop_drawing"
+    @pointermove="move_draw"
+  )
+    g.no_events.preview
+      g.circles
+        circle(v-for="p in points_to_draw" :cx="p.x" :cy="p.y" r="2" fill="white" stroke="red" stroke-width="1")
+      path(:d="d" stroke-width="1" stroke="blue" fill="transparent")
 </template>
 
 <style scoped lang="less">
@@ -22,4 +66,7 @@ svg.operation {
   .canvas_svg();
 }
 
+g.no_events {
+  pointer-events: none;
+}
 </style>
