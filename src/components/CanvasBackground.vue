6f2328480e6cbc7f4fe9b7ef328c4e89dae4e108
kotobukid <kotobuki@daqua.jp>
2024-07-05 15:54:05 +0900

    背景レイヤーを作る

---
+++ src/components/CanvasBackground.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+
+const canvas = ref(null);
+
+onMounted(() => {
+  const ctx: CanvasRenderingContext2D = canvas.value.getContext("2d");
+
+  ctx.fillStyle = 'red';
+  ctx.strokeStyle = 'pink';
+
+  ctx.fillRect(25, 25, 100, 100);
+  ctx.clearRect(45, 45, 60, 60);
+  ctx.strokeRect(50, 50, 50, 50);
+});
+</script>
+
+<template lang="pug">
+  canvas.background(ref="canvas" width="1000" height="600")
+</template>
+
+<style scoped>
+canvas.background {
+  outline: 1px solid grey;
+}
+</style>
